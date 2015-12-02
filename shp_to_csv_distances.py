0680671a5181c2d772377d1270bcfc332599ab33
tayden-hakai <tayden.denouden@hakai.org>
2015-12-02 10:08:20 -0800

    Add a couple global variables to simplify the map call. Update function docstrings

--- shp_to_csv_distances.py
+++ shp_to_csv_distances.py
@@ -17,25 +17,22 @@ def extract_ids(input_file):
         return [shp['id'] for shp in source]
 
 
-def calculate_distances(args):
-    """Calculate the distances between all `ids1` to `ids2` in `infile`.
+def calculate_distances(shp_id):
+    """Calculate distances between `shp_id` and all other features in shapefile.
 
-    infile should be a shapefile with features, ids1 and ids2 should be
-    a list of shape ids you wish to have distances calculated for.
-    Distances between polygons that are both in ids1 will not be calculated
-    unless they are also in ids2 and vice versa. This allows the number of
-    distance queries to be limited.
-    q is a multiprocessing q object to which the returned data is placed so
-    that it may be accessed from the calling process.
+    `shp_id` is the feature id in the shapefile `infile` (global). `shp_id` and
+    each id in `ids` (global) are cast to shapely features and then passed to a
+    distance function contained in the shapely library. An array is returned
+    with all these distances.
     """
-    with fiona.open(args['infile']) as source:
+    with fiona.open(infile) as source:
         source = list(source)
         result = []
 
         # Calculate distances and store in result
-        i_shp = shape(source[int(args['shp_id'])]['geometry'])
-        for j in args['ids']:
-            if int(j) < int(args['shp_id']):
+        i_shp = shape(source[int(shp_id)]['geometry'])
+        for j in ids:
+            if int(j) < int(shp_id):
                 result.append(-1)
                 continue
 
@@ -48,25 +45,21 @@ def calculate_distances(args):
         return result
 
 
-def main():
+if __name__ == "__main__":
     """Main execution thread."""
-    # infile = "./data/random_points/test_polys.shp"
-    infile = "./data/low_water_final/low_water.shp"
+    infile = "./data/random_points/test_polys.shp"
+    # infile = "./data/low_water_final/low_water.shp"
     ids = extract_ids(infile)
 
-    # Calculate each the distance from each id to ids using a process pool
     print "Calculating distances"
-    pool = Pool()
-    data = pool.map(calculate_distances, [{
-        'shp_id': shp_id,
-        'infile': infile,
-        'ids': ids,
-    } for shp_id in ids], chunksize=100)
+    # Map each id to a list of distances to all other ids
+    pool = Pool()  # Spawn a worker process for each cpu
+    data = pool.map(calculate_distances, ids, chunksize=100)
 
     # Write the data to a new csv file
     outfile = open("test.csv", "w")
 
-    # Write header of output file
+    # Write header
     print "Writing Header"
     outfile.write("NODE")
     for i in ids:
@@ -85,6 +78,3 @@ def main():
         outfile.write("\n")
 
     outfile.close()
-
-if __name__ == "__main__":
-    main()
