d6e5a34ece867e1294439a23b53dd6c6b87a810e
tayden-hakai <tayden.denouden@hakai.org>
2015-12-02 13:47:31 -0800

    Prevent opening shapefile multiple times

--- shp_to_csv_distances.py
+++ shp_to_csv_distances.py
@@ -6,6 +6,7 @@ Written by: Taylor Denouden
 Date: November 25, 2015
 """
 
+import sys
 import fiona
 from shapely.geometry import shape
 from multiprocessing import Pool
@@ -25,24 +26,26 @@ def calculate_distances(args):
     distance function contained in the shapely library. An array is returned
     with all these distances.
     """
-    with fiona.open(args['infile']) as source:
-        source = list(source)
-        result = []
+    i = args[0][0]
+    i_shp = args[0][1]
+    shps = args[1]
+    result = []
 
-        # Calculate distances and store in result
-        i_shp = shape(source[int(args['shp_id'])]['geometry'])
-        for j in args['ids']:
-            if int(j) < int(args['shp_id']):
-                result.append(-1)
-                continue
+    # Calculate distances and store in result
+    for (j, j_shp) in shps:
+        if int(j) < int(i):
+            dist = -1
+        else:
+            try:
+                dist = i_shp.distance(j_shp)
+            except Exception as e:
+                with open(sys.path.join("logs", i, ".txt", "w+")) as logfile:
+                    logfile.write(e + "\n")
+                dist = -2
 
-            j_shp = shape(source[int(j)]['geometry'])
-            if i_shp.is_valid and j_shp.is_valid:
-                result.append(i_shp.distance(j_shp))
-            else:
-                result.append(-1)
+        result.append(dist)
 
-        return result
+    return result
 
 
 def main():
@@ -50,15 +53,17 @@ def main():
     # infile = "./data/random_points/test_polys.shp"
     infile = "./data/low_water_final/low_water.shp"
     ids = extract_ids(infile)
+    shapes = []
+
+    # Get all shapefiles in memory as shapely shapes
+    with fiona.open(infile) as source:
+        source = list(source)
+        shapes = [(i, shape(source[int(i)]['geometry'])) for i in ids]
 
     # Calculate each the distance from each id to ids using a process pool
     print "Calculating distances"
     pool = Pool()
-    data = pool.map(calculate_distances, [{
-        'shp_id': shp_id,
-        'infile': infile,
-        'ids': ids,
-    } for shp_id in ids], chunksize=100)
+    data = pool.map(calculate_distances, [(i, shapes) for i in shapes], chunksize=50)
 
     # Write the data to a new csv file
     outfile = open("test.csv", "w")
